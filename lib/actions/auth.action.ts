a3279821f991693288d001d493160df52624f01e
Norman Cade <norman_cade32@yahoo.com>
2025-05-22 07:58:45 -0400

    Can sign in and sign out

---
+++ lib/actions/auth.action.ts
@@ -0,0 +1,80 @@
+"use server";
+
+import { db, auth } from "@/firebase/admin";
+import { cookies } from "next/headers";
+
+const ONE_WEEK = 60 * 60 * 24 * 7;
+
+export async function signUp(params: SignUpParams) {
+  const { name, uid, email } = params;
+
+  try {
+    const userRecord = await db.collection("users").doc(uid).get();
+    if (userRecord.exists) {
+      return {
+        success: false,
+        message: "User already exists. Please Sign In.",
+      };
+    }
+
+    await db.collection("users").doc(uid).set({ name, email });
+    return {
+      success: true,
+      message: "Account created successfully. Please sign in.",
+    };
+  } catch (error: any) {
+    console.error("Error signing up:", error);
+
+    if (error.code === "auth/email-already-in-use") {
+      return {
+        success: false,
+        message: "Email already in use",
+      };
+    }
+
+    return {
+      success: false,
+      message: "Error signing up",
+    };
+  }
+}
+
+export async function signIn(params: SignInParams) {
+  const { email, idToken } = params;
+
+  try {
+    const userRecord = await auth.getUserByEmail(email);
+    if (!userRecord) {
+      return {
+        success: false,
+        message: "User not found. Please sign up.",
+      };
+    }
+    await setSessionCookie(idToken);
+  } catch (error) {
+    console.log("Error signing in:", error);
+    return {
+      success: false,
+      message: "Error signing in",
+    };
+  }
+}
+
+export async function setSessionCookie(idToken: string) {
+  try {
+    const cookieStore = await cookies();
+    const sessionCookie = await auth.createSessionCookie(idToken, { expiresIn: ONE_WEEK * 1000 });
+    cookieStore.set("session", sessionCookie, {
+      maxAge: ONE_WEEK,
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      path: "/",
+      sameSite: "lax",
+    });
+
+    return sessionCookie;
+  } catch (error) {
+    console.error("Error creating session cookie:", error);
+    throw new Error("Error creating session cookie");
+  }
+}
