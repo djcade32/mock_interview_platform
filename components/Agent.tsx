59002b1f260d71daf776312d6203b8cd54555fc3
Norman Cade <norman_cade32@yahoo.com>
2025-05-24 08:47:05 -0400

    Storing feedback in db

--- components/Agent.tsx
+++ components/Agent.tsx
@@ -1,12 +1,12 @@
 "use client";
 
 import { generator, interviewer } from "@/constants";
+import { createFeedback } from "@/lib/actions/general.action";
 import { cn } from "@/lib/utils";
 import { vapi } from "@/lib/vapi.sdk";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import React, { useEffect, useState } from "react";
-import { set } from "react-hook-form";
 
 enum CallStatus {
   INACTIVE = "INACTIVE",
@@ -70,10 +70,11 @@ const Agent = ({ userName, userId, type, interviewId, questions }: AgentProps) =
 
   const handleGenerateFeedback = async (messages: SavedMessages[]) => {
     console.log("Generate Feedback here");
-    const { success, id } = {
-      success: true,
-      id: "feedback-id-12345",
-    };
+    const { success, feedbackId: id } = await createFeedback({
+      interviewId: interviewId!,
+      userId: userId!,
+      transcript: messages,
+    });
 
     if (success && id) {
       router.push(`/interview/${interviewId}/feedback`);
@@ -88,8 +89,8 @@ const Agent = ({ userName, userId, type, interviewId, questions }: AgentProps) =
 
     if (type === "generate") {
       await vapi.start(generator, {
-        clientMessages: [],
-        serverMessages: [],
+        clientMessages: undefined,
+        serverMessages: undefined,
         variableValues: {
           username: userName,
           userid: userId,
@@ -102,8 +103,8 @@ const Agent = ({ userName, userId, type, interviewId, questions }: AgentProps) =
       }
 
       await vapi.start(interviewer, {
-        clientMessages: [],
-        serverMessages: [],
+        clientMessages: undefined,
+        serverMessages: undefined,
         variableValues: {
           questions: formattedQuestions,
         },
